a5740bc54c8634d36fef6d34e7e9177da65b7809
MellowMancer <wyatharth@gmail.com>
2023-06-10 13:26:17 +0530

    Made a title screen and added multiplayer

--- Pong using OpenCV/pong.py
+++ Pong using OpenCV/pong.py
@@ -1,6 +1,186 @@
 import cv2
 import numpy as np
 
+def single_player(m, n, ux, uy, flagx, flagy, flag, l, cap, fps):
+
+    # Setting certain properties for the video capture
+    #   3: frameWidth
+    #   4: frameHeight
+    #   5: frameRate
+    cap.set(3, frameWidth)
+    cap.set(4, frameHeight)
+    cap.set(5, fps)
+    t = 0
+        # while loop to make sure the frame is refreshed regularly
+    while True:
+        success, img = cap.read()
+        # Creating a canvas and initializing its colour to a shade of blue
+        canvas = np.zeros((int(frameHeight),int(frameWidth),3), np.uint8)
+        canvas[:] = (50, 30, 30)
+        # canvas = img
+
+        # Flipping the image read by the webcam so all the movement is mirrored
+        img = cv2.flip(img, 1)
+
+        # Converting the image to greyscale for later use in face recognition
+        blur = cv2.GaussianBlur(img,(5,5),0)
+        grey = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+
+        # Detecting all the faces in the image
+        face = faceCascade.detectMultiScale(grey, 1.1, 4)
+        # Specifying boundary conditions so that the ball stays in the canvas
+        if((m+3 >= frameWidth - 30) or (m-3 <= 30)) and flagx == 0:
+            ux = -ux
+            flagx = 1
+        else:
+            flagx = 0
+        if((n+3 >= frameHeight - 30) or (n-3 <= 30)) and flagy == 0:
+            uy = -uy
+            flagy = 1
+        else:
+            flagy = 0
+
+        # Creating the ball and updating its position with each iteration
+        cv2.circle(canvas, (int(m), int(n)), 4, (255,255,255), 10)
+        
+        # Used to create a "racket" for each face recognized
+        for (x,y,w,h) in face:
+            cv2.rectangle (canvas, (0,int((2*y+h)/2)-l), (3, int((2*y+h)/2)+l), (255,255,255), 5)
+
+            # Increment the points if the ball hits the racket, otherwise set to 0
+            if(m-3 <= 30) and ((n+10 >= int((2*y+h)/2)-l) and (n-10 <= int((2*y+h)/2)+l)) and flag == 0:
+                t = t+1
+                flag = 1 
+            elif(m-3 <= 30) and flag == 0:
+                t = 0
+                flag = 1
+            else:
+                flag = 0
+
+        # Displaying the points and pausing the point counter if no face is detected
+        if(face == ()):
+            cv2.putText(canvas, "Face not detected: Point counter paused", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), 1, cv2.LINE_AA)
+            cv2.putText(canvas, str(t), (int(frameWidth/2-40), 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (150,150,150), 4, cv2.LINE_AA)
+        else:
+            cv2.putText(canvas, str(t), (int(frameWidth/2-40), 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,255), 4, cv2.LINE_AA)
+
+        # Updating coordinates of ball
+        m = m+ux
+        n = n+uy
+
+        # Rendering the frame
+        cv2.imshow("Pong", canvas)
+
+        # Game is paused if 'p' is pressed and it terminates if 'q' is pressed
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('p'):
+            canvas1 = canvas
+            cv2.putText(canvas1, "PAUSED", (int(340), int(frameHeight/2)), cv2.FONT_HERSHEY_SIMPLEX, 3, (150,150,150), 4, cv2.LINE_AA)
+            cv2.imshow("Pong", canvas1)
+            while(cv2.waitKey(1) & 0xFF != ord('p')):
+                temp = 1
+        elif key == ord('q'):
+            break
+
+def multi_player(m, n, ux, uy, flagx, flagy, flag, l, cap, fps):
+
+    # Setting certain properties for the video capture
+    #   3: frameWidth
+    #   4: frameHeight
+    #   5: frameRate
+    cap.set(3, frameWidth)
+    cap.set(4, frameHeight)
+    cap.set(5, fps)
+    t1 = 0
+    t2 = 0
+        # while loop to make sure the frame is refreshed regularly
+    while True:
+        success, img = cap.read()
+        # Creating a canvas and initializing its colour to a shade of blue
+        canvas = np.zeros((int(frameHeight),int(frameWidth),3), np.uint8)
+        canvas[:] = (50, 30, 30)
+        # canvas = img
+
+        # Flipping the image read by the webcam so all the movement is mirrored
+        img = cv2.flip(img, 1)
+
+        # Converting the image to greyscale for later use in face recognition
+        blur = cv2.GaussianBlur(img,(5,5),0)
+        grey = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+
+        # Detecting all the faces in the image
+        face = faceCascade.detectMultiScale(grey, 1.1, 4)
+        # Specifying boundary conditions so that the ball stays in the canvas
+        if((m+3 >= frameWidth - 30) or (m-3 <= 30)) and flagx == 0:
+            ux = -ux
+            flagx = 1
+        else:
+            flagx = 0
+        if((n+3 >= frameHeight - 30) or (n-3 <= 30)) and flagy == 0:
+            uy = -uy
+            flagy = 1
+        else:
+            flagy = 0
+
+        # Creating the ball and updating its position with each iteration
+        cv2.circle(canvas, (int(m), int(n)), 4, (255,255,255), 10)
+        cv2.line(canvas, (int(frameWidth//2), 0), (int(frameWidth//2), int(frameHeight)), (255,255,255), 1)
+        
+        # Used to create a "racket" for each face recognized
+        for (x,y,w,h) in face:
+            if x+w < int(frameWidth/2):
+                cv2.rectangle (canvas, (0,int((2*y+h)/2)-l), (3, int((2*y+h)/2)+l), (255,255,255), 5)
+
+                # Increment the points of other player if the ball misses
+                if(m-3 <= 30) and ((n+10 >= int((2*y+h)/2)-l) and (n-10 <= int((2*y+h)/2)+l)) and flag == 0:
+                    flag = 1 
+                elif(m-3 <= 30) and flag == 0:
+                    t2 +=1
+                    flag = 1
+                else:
+                    flag = 0
+            else:
+                cv2.rectangle (canvas, (int(frameWidth-3),int((2*y+h)/2)-l), (int(frameWidth), int((2*y+h)/2)+l), (255,255,255), 5)
+
+                # Increment the points of other player if the ball misses
+                if(m+3 >= frameWidth-30) and ((n+10 >= int((2*y+h)/2)-l) and (n-10 <= int((2*y+h)/2)+l)) and flag == 0:
+                    flag = 1 
+                elif(m+3 >= frameWidth-30) and flag == 0:
+                    t1 +=1
+                    flag = 1
+                else:
+                    flag = 0
+
+
+        # Displaying the points and pausing the point counter if no face is detected
+        if(face == ()):
+            cv2.putText(canvas, "Face not detected: Point counters paused", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), 1, cv2.LINE_AA)
+            cv2.putText(canvas, str(t1), (int(frameWidth/2-140), 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (150,150,150), 4, cv2.LINE_AA)
+            cv2.putText(canvas, str(t2), (int(frameWidth/2+120), 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (150,150,150), 4, cv2.LINE_AA)
+        else:
+            cv2.putText(canvas, str(t1), (int(frameWidth/2-140), 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,255), 4, cv2.LINE_AA)
+            cv2.putText(canvas, str(t2), (int(frameWidth/2+120), 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,255), 4, cv2.LINE_AA)
+
+        # Updating coordinates of ball
+        m = m+ux
+        n = n+uy
+
+        # Rendering the frame
+        cv2.imshow("Pong", canvas)
+
+        # Game is paused if 'p' is pressed and it terminates if 'q' is pressed
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('p'):
+            canvas1 = canvas
+            cv2.putText(canvas1, "PAUSED", (int(340), int(frameHeight/2)), cv2.FONT_HERSHEY_SIMPLEX, 3, (150,150,150), 4, cv2.LINE_AA)
+            cv2.imshow("Pong", canvas1)
+            while(cv2.waitKey(1) & 0xFF != ord('p')):
+                temp = 1
+        elif key == ord('q'):
+            break
+
+
+
 # Setting a custom frame width
 frameWidth = 1080
 frameHeight = 9*frameWidth/16
@@ -11,14 +191,6 @@ faceCascade = cv2.CascadeClassifier("Pong using OpenCV\haarcascade_frontalface_a
 # Capturing video from webcam
 cap = cv2.VideoCapture(0)
 fps = cap.get(cv2.CAP_PROP_FPS)
-
-# Setting certain properties for the video capture
-#   3: frameWidth
-#   4: frameHeight
-#   5: frameRate
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
-cap.set(5, fps)
 
 # Setting initial parameters for the ball
 #   m: Current x-coordinate
@@ -34,86 +206,57 @@ m = 100
 n = 100
 ux = 100
 uy = 50
-t = 0
 flagx = 0
 flagy = 0
 flag = 0
-flagp = 0
 l = 120
 # ax = 2
 # ay = 2
+cursor = 0
 
-# Allowing the user to customise the parameters of the game
-update = input("Would you like to change the default settings for ball speed and bar length? (y/n): ")
-if(update=='y' or update=='yes'):
-    ux = int(input("Enter the ball speed (Default: 100): "))
-    l = int(input("Enter the length of the bar (Default: 120): "))
-# while loop to make sure the frame is refreshed regularly
+#Title Screen
 while True:
-    success, img = cap.read()
-    # Creating a canvas and initializing its colour to a shade of blue
     canvas = np.zeros((int(frameHeight),int(frameWidth),3), np.uint8)
     canvas[:] = (50, 30, 30)
-
-    # Flipping the image read by the webcam so all the movement is mirrored
-    img = cv2.flip(img, 1)
-
-    # Converting the image to greyscale for later use in face recognition
-    blur = cv2.GaussianBlur(img,(5,5),0)
-    grey = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-
-    # Detecting all the faces in the image
-    face = faceCascade.detectMultiScale(grey, 1.1, 4)
-    # Specifying boundary conditions so that the ball stays in the canvas
-    if((m+3 >= frameWidth - 30) or (m-3 <= 30)) and flagx == 0:
-        ux = -ux
-        flagx = 1
-    else:
-        flagx = 0
-    if((n+3 >= frameHeight - 30) or (n-3 <= 30)) and flagy == 0:
-        uy = -uy
-        flagy = 1
-    else:
-        flagy = 0
-
-    # Creating the ball and updating its position with each iteration
-    cv2.circle(canvas, (int(m), int(n)), 4, (255,255,255), 10)
     
-    # Used to create a "racket" for each face recognized
-    for (x,y,w,h) in face:
-        cv2.rectangle (canvas, (0,int((2*y+h)/2)-l), (3, int((2*y+h)/2)+l), (255,255,255), 5)
+    cv2.putText(canvas, "PONG", (int(370), int(frameHeight/2)-80), cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,255), 4, cv2.LINE_AA)
+    options = ["Rally", "1v1"]
+    key = cv2.waitKey(1) & 0xFF
 
-        # Increment the points if the ball hits the racket, otherwise set to 0
-        if(m-3 <= 30) and ((n+10 >= int((2*y+h)/2)-l) and (n-10 <= int((2*y+h)/2)+l)) and flag == 0:
-            t = t+1
-            flag = 1 
-        elif(m-3 <= 30) and flag == 0:
-            t = 0
-            flag = 1
-        else:
-            flag = 0
-
-    # Displaying the points and pausing the point counter if no face is detected
-    if(face == ()):
-        cv2.putText(canvas, "Face not detected: Point counter paused", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), 1, cv2.LINE_AA)
-        cv2.putText(canvas, str(t), (int(frameWidth/2-40), 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (150,150,150), 4, cv2.LINE_AA)
-    else:
-        cv2.putText(canvas, str(t), (int(frameWidth/2-40), 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,255), 4, cv2.LINE_AA)
-
-    # Updating coordinates of ball
-    m = m+ux
-    n = n+uy
-
+    if cursor == 0:
+        cv2.putText(canvas, "Rally", (860,int(frameHeight/2)+10), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 3, cv2.LINE_AA)
+        cv2.putText(canvas, "1v1", (860,int(frameHeight/2)+80), cv2.FONT_HERSHEY_SIMPLEX, 2, (80, 60, 60), 3, cv2.LINE_AA)
+        cv2.putText(canvas, "Quit", (860,int(frameHeight/2)+150), cv2.FONT_HERSHEY_SIMPLEX, 2, (80, 60, 60), 3, cv2.LINE_AA)
+    elif cursor == 1:
+        cv2.putText(canvas, "Rally", (860,int(frameHeight/2)+10), cv2.FONT_HERSHEY_SIMPLEX, 2, (80, 60, 60), 3, cv2.LINE_AA)
+        cv2.putText(canvas, "1v1", (860,int(frameHeight/2)+80), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 3, cv2.LINE_AA)
+        cv2.putText(canvas, "Quit", (860,int(frameHeight/2)+150), cv2.FONT_HERSHEY_SIMPLEX, 2, (80, 60, 60), 3, cv2.LINE_AA)
+    elif cursor == 2:
+        cv2.putText(canvas, "Rally", (860,int(frameHeight/2)+10), cv2.FONT_HERSHEY_SIMPLEX, 2, (80, 60, 60), 3, cv2.LINE_AA)
+        cv2.putText(canvas, "1v1", (860,int(frameHeight/2)+80), cv2.FONT_HERSHEY_SIMPLEX, 2, (80, 60, 60), 3, cv2.LINE_AA)
+        cv2.putText(canvas, "Quit", (860,int(frameHeight/2)+150), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 3, cv2.LINE_AA)
+    
+    if key == ord('w'):
+        if cursor != 0:
+            # cv2.rectangle(canvas, (850,int(frameHeight/2)+cursor*70), (frameWidth, int(frameHeight/2)+(cursor-1)*70), (80, 60, 60), 5)
+            # cv2.floodFill(canvas, (880, None, int(frameHeight/2)+cursor*70+50), (80, 60, 60))
+            cursor -= 1
+    if key == ord('s'):
+        if cursor != 2:
+            # cv2.rectangle(canvas, (850,int(frameHeight/2)+cursor+1*70), (frameWidth, int(frameHeight/2)+(cursor+2)*70), (80, 60, 60), 5)
+            # cv2.floodFill(canvas, (880, None, int(frameHeight/2)+(cursor+1)*70+50), (80, 60, 60))
+            cursor += 1
+    elif key == 32:
+        if cursor == 0:
+            single_player(m,n,ux,uy,flagx,flagy,flag,l, cap, fps)
+        elif cursor == 1:
+            multi_player(m,n,ux,uy,flagx,flagy,flag,l, cap, fps)
+        break
     # Rendering the frame
     cv2.imshow("Pong", canvas)
 
-    # Game is paused if 'p' is pressed and it terminates if 'q' is pressed
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('p'):
-        canvas1 = canvas
-        cv2.putText(canvas1, "PAUSED", (int(340), int(frameHeight/2)), cv2.FONT_HERSHEY_SIMPLEX, 3, (150,150,150), 4, cv2.LINE_AA)
-        cv2.imshow("Pong", canvas1)
-        while(cv2.waitKey(1) & 0xFF != ord('p')):
-            temp = 1
-    elif key == ord('q'):
-        break
+# Allowing the user to customise the parameters of the game
+# update = input("Would you like to change the default settings for ball speed and bar length? (y/n): ")
+# if(update=='y' or update=='yes'):
+#     ux = int(input("Enter the ball speed (Default: 100): "))
+#     l = int(input("Enter the length of the bar (Default: 120): "))
